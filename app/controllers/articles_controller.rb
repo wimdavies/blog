8bda460fc6be23879f22f63d67c08e35e4bbcd3b
wimdavies <william.johnston.davies@gmail.com>
2023-10-19 15:36:09 +0100

    7.4 Updating an Article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -17,7 +17,21 @@ class ArticlesController < ApplicationController
     if @article.save
       redirect_to @article
     else
-      render :new, status: :unprocessable_entity 
+      render :new, status: :unprocessable_entity
+    end
+  end
+  
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render :edit, status: :unprocessable_entity
     end
   end
 
