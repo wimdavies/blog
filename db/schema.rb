e1f40985bb568b038f009102a3065ffe5f3bff73
wimdavies <william.johnston.davies@gmail.com>
2023-10-22 11:31:26 +0100

    9.3 Using Concerns: before refactor into Visible concern

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_10_20_141707) do
+ActiveRecord::Schema[7.1].define(version: 2023_10_22_100149) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "status", default: "public"
   end
 
   create_table "comments", force: :cascade do |t|
@@ -24,6 +25,7 @@ ActiveRecord::Schema[7.1].define(version: 2023_10_20_141707) do
     t.integer "article_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "status", default: "public"
     t.index ["article_id"], name: "index_comments_on_article_id"
   end
 
